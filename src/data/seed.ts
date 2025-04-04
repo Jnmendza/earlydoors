e6025f5fc23b6d2e30cf64d89c486f952234e88c
Jonathan Mendoza <jonatmendoza@gmail.com>
2025-04-04 01:06:04 -0700

    connected prisam and supabase

---
+++ src/data/seed.ts
@@ -0,0 +1,13 @@
+// INSERT INTO teams (id, name, logo_url, league, country)
+// VALUES
+//   (gen_random_uuid(), 'FC Barcelona', 'https://upload.wikimedia.org/wikipedia/en/4/47/FC_Barcelona_%28crest%29.svg', 'La Liga', 'Spain'),
+//   (gen_random_uuid(), 'Mexico National Team', 'https://upload.wikimedia.org/wikipedia/en/3/3f/Mexico_national_football_team_crest.svg', 'International', 'Mexico'),
+//   (gen_random_uuid(), 'San Diego FC', 'https://upload.wikimedia.org/wikipedia/en/6/6f/San_Diego_FC_logo.svg', 'MLS', 'USA');
+//   (gen_random_uuid(), 'Manchester United F.C', 'https://upload.wikimedia.org/wikipedia/en/7/7a/Manchester_United_FC_crest.svg', 'Premier League', 'England');
+
+// INSERT INTO supporters_groups (id, team_id, name, group_logo_url, city, description, ig_handle, website)
+// VALUES
+//   (gen_random_uuid(), '72e2eb04-18ff-4a0d-866c-f16f29772e2b', 'Penya Blaugrana San Diego', 'https://www.wegotsoccer.com/mmWGS/team/logos/penya-logo.png', 'San Diego', 'Official FC Barcelona supporters group in SD.', '@penyablausd', 'https://penyablausd.com'),
+//   (gen_random_uuid(), 'd3f0ea3f-111f-48f3-83d5-4138546e2fef', 'Mexico Fans SD', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrCreqUuoVl1TMtueijJhQFc1lR59Z8i8G7A&s', 'San Diego', 'Local supporters of El Tri — the Mexican National Team.', '@mexicofanssd', NULL),
+//   (gen_random_uuid(), '12f2bcc5-dfe6-4322-adee-143392f5ae32', 'Chavos Frontera Ultras', NULL, 'San Diego', 'Community-driven support for San Diego FC.', '@fronteraultras', NULL),
+//   (gen_random_uuid(), '1d9c9f7d-2aa3-4be8-a271-21f742df5c11', 'San Diego RedDevils', NULL, 'San Diego', 'Community-driven support for M.U.F.C.', '@reddevilssd', NULL)
