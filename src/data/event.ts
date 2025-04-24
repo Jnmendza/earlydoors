ea662b2be7cdae19ef7e10e5447ca3e3f3707981
Jonathan Mendoza <jonatmendoza@gmail.com>
2025-04-23 22:41:38 -0700

    added calendar and dialog box to overview page

--- src/data/event.ts
+++ src/data/event.ts
@@ -4,13 +4,17 @@ export const getEvents = async () => {
   try {
     const events = await db.event.findMany({
       include: {
-        Venue: true,
+        Venue: {
+          select: {
+            name: true,
+          },
+        },
         Club: true,
       },
     });
     return events;
   } catch (error) {
-    console.error("Erro fetching events:", error);
+    console.error("Error fetching events:", error);
     throw new Error("Failed to fetch events");
   }
 };
--- src/types/types.ts
+++ src/types/types.ts
@@ -1,1 +1,12 @@
 export type ModelType = "event" | "venue" | "club" | "supportersGroup";
+
+export type CalendarEvent = {
+  id: string;
+  title: string;
+  start: Date;
+  end: Date;
+  venueId: string;
+  allDay?: boolean;
+  desc?: string;
+  venueName: string;
+};
